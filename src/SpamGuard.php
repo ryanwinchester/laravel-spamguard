a673a2214d1c6e6bff51fce734bb9034c1f74051
fungku <fungku@gmail.com>
2015-07-04 23:02:46 -0700

    fill out spamguard class a bit

--- src/SpamGuard.php
+++ src/SpamGuard.php
@@ -2,11 +2,28 @@
 
 namespace Fungku\SpamGuard;
 
+use Fungku\SpamGuard\Services\HtmlGenerator;
+use Fungku\SpamGuard\Services\MiddlewareAssigner;
+
 class SpamGuard
 {
-    public function html()
-    {}
+    /**
+     * @param array $options
+     * @return string
+     * @throws Exceptions\SpamGuardException
+     */
+    public function html($options = [])
+    {
+        return HtmlGenerator::generate($options);
+    }
 
-    public function middleware()
-    {}
+    /**
+     * @param $controller
+     * @param array $options
+     * @return void
+     */
+    public function middleware($controller, $options = [])
+    {
+        MiddlewareAssigner::assign($controller, $options);
+    }
 }
